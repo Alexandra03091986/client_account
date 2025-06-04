e8f412d1e2b8bd34cfb0a21ebecef4bbd4502de6
shura_1986@mail.ru <shura_1986@mail.ru>
2025-06-04 22:36:27 +0300

    masking bank card and account numbers

---
+++ src/masks.py
@@ -0,0 +1,17 @@
+def get_mask_card_number(card_number: str) -> str:
+    ''' Функция маскировки номера банковской карты '''
+    mask_card_number = card_number[:4] + ' ' + card_number[4:6] + '** **** ' + card_number[-4:]
+    return mask_card_number
+
+
+def get_mask_account(number_account: str) -> str:
+    ''' Функция маскировки номера банковского счета '''
+    mask_account = '**' + number_account[-4:]
+    return mask_account
+
+
+if __name__ == "__main__":
+    print(get_mask_card_number('7000792289606361'))
+
+if __name__ == "__main__":
+    print(get_mask_account('73654108430135874305'))
