15b7527acbcbff725af180efc41947c89901b5d2
shura_1986@mail.ru <shura_1986@mail.ru>
2025-06-05 08:53:10 +0300

    add function mask account card

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card
 
 result = get_mask_card_number('7000792289606361')
 print(result)
 
 result = get_mask_account('73654108430135874305')
 print(result)
+
+result = mask_account_card('Visa Platinum 7000792289606361')
+print(mask_account_card)
---
+++ src/widget.py
@@ -0,0 +1,27 @@
+def mask_account_card(proces_inform: str) -> str:
+    ''' Функция обрабатывает информацию как о картах, так и о счетах '''
+    len_list = len(proces_inform)
+    if len_list == 30:
+        mask_numbers = proces_inform[:4] + ' ' + proces_inform[5:13] + ' ' + proces_inform[14:18] + ' ' + proces_inform[19:21] + '** **** ' + proces_inform[-4:]
+    if len_list == 24:
+        mask_numbers = proces_inform[:7] + ' ' + proces_inform[8:12] + ' ' + proces_inform[13:15] + '** **** ' + proces_inform[-4:]
+    if len_list == 25:
+        mask_numbers = proces_inform[:5] + '**' + proces_inform[-4:]
+    return mask_numbers
+
+
+def get_date(formatting_date: str) -> str:
+    '''  Функция меняет формат даты '''
+    data = formatting_date[:10]
+    new_data = []
+    for element in data.split('-'):
+        new_data.append(element)
+        reversed_new_data = new_data[::-1]
+    return '.'.join(reversed_new_data)
+
+
+if __name__ == "__main__":
+    print(mask_account_card('Visa Platinum 7000792289606361'))
+    print(mask_account_card('Maestro 7000792289606361'))
+    print(mask_account_card('Счет 73654108430135874305'))
+    print(get_date("2024-03-11T02:26:18.671407"))
