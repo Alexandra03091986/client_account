96b78768f3d2384b3b71b21f6d67c666cc01421d
shura_1986@mail.ru <shura_1986@mail.ru>
2025-06-16 01:10:11 +0300

    using parameterization in test processing

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,9 +1,21 @@
+import pytest
+
+
 from src.processing import filter_by_state, sort_by_date
 
 
-def test_filter_by_state():
-    assert filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+@pytest.mark.parametrize("state, expected", [("EXECUTED", [41428829, 594226727]),
+                                             ("CANCELED", [939719570]),
+                                             ("PENDING", [615064591]),
+                                             ("UNKNOWN", []),
+                                             ])
+def test_filter_by_state(card_operations, state, expected):
+    result = filter_by_state(card_operations, state)
+    assert [t["id"] for t in result] == expected
 
 
-def test_sort_by_date():
+@pytest.mark.parametrize("reverse, expected", [(True, 41428829),
+                                            (False, 939719570),
+                                            ])
+def test_sort_by_date(card_operations, reverse, expected):
     assert sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
