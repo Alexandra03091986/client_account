8a3a40ae6b53c90228bc771260d68b4b76a8face
shura_1986@mail.ru <shura_1986@mail.ru>
2025-06-15 12:55:35 +0300

    writing and application fixture for tests

--- src/processing.py
+++ src/processing.py
@@ -2,9 +2,9 @@ from typing import Iterable
 
 
 def filter_by_state(list_of_dictionaries: list, state: str = 'EXECUTED') -> Iterable:
-    ''' Функция, которая принимает список словарей и опционально значение для ключа state
+    """ Функция, которая принимает список словарей и опционально значение для ключа state
     (по умолчанию 'EXECUTED'). Функция возвращает новый список словарей, содержащий только те
-    словари, у которых ключ state соответствует указанному значению. '''
+    словари, у которых ключ state соответствует указанному значению. """
     filtered_list = []
     for dictionary in list_of_dictionaries:
         if dictionary['state'] == state:
@@ -13,9 +13,9 @@ def filter_by_state(list_of_dictionaries: list, state: str = 'EXECUTED') -> Iter
 
 
 def sort_by_date(list_of_dictionaries: Iterable, decreasing: bool = True) -> Iterable:
-    ''' Функция, которая принимает список словарей и необязательный параметр,
+    """ Функция, которая принимает список словарей и необязательный параметр,
     задающий порядок сортировки (по умолчанию — убывание). Функция должна возвращать
-     новый список, отсортированный по дате (date).'''
+     новый список, отсортированный по дате (date)."""
     sorted_list_by_data = sorted(list_of_dictionaries, key = lambda list_of_dic: int(list_of_dic['date'].split('T')[0].replace('-','')), reverse=decreasing)
     return sorted_list_by_data
 
