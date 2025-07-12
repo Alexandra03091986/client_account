1847781829667252f50b7cc8c03113ab7d2bbc06
shura_1986@mail.ru <shura_1986@mail.ru>
2025-07-12 21:30:25 +0300

    creating function for reading transactions from XLSX

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.masks import get_mask_account, get_mask_card_number
 from src.utils import get_amount_transactions_in_rub, get_list_dict_finance_transactions
 from config import PATH_JSON
+from src.file_transactions import get_transactions_file_csv, get_transactions_file_xlsx, PATH_XLSX, PATH_CSV
 
 result = get_mask_card_number("7000792289606361")
 print(result)
@@ -11,3 +12,8 @@ print(result)
 transactions = get_list_dict_finance_transactions(PATH_JSON)
 print(transactions)  # Выведет список транзакций или [] в случае ошибки
 print(get_amount_transactions_in_rub(transactions[2]))
+
+csv_transactions = get_transactions_file_csv(PATH_CSV)
+print(csv_transactions)
+xlsx_transactions = get_transactions_file_xlsx(PATH_XLSX)
+print(xlsx_transactions)
--- src/file_transactions.py
+++ src/file_transactions.py
@@ -1,17 +1,19 @@
-import csv
+import os
 import pandas as pd
+from config import DATA_DIR
 
+
+PATH_CSV = os.path.join(DATA_DIR, 'transactions.csv')
+PATH_XLSX = os.path.join(DATA_DIR, 'transactions_excel.xlsx')
 
 def get_transactions_file_csv(file_csv):
     """ Функция для считывания финансовых операций из CSV.
      Возвращает список словарей с транзакцией."""
-    transactions = []
+
     try:
         with open(file_csv, encoding='utf-8') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                transactions.append(row)
-        return transactions
+            excel_transaction = pd.read_csv(file, delimiter=';')
+            return excel_transaction.to_dict("records")
     except FileNotFoundError:
         print(f"Ошибка: файл {file_csv} не найден! ")
         return []
@@ -24,29 +26,20 @@ def get_transactions_file_xlsx(file_xlsx):
     """ Функция для считывания финансовых операций из Excel.
     Возвращает список словарей с транзакцией."""
 
-    # print(excel_transaction.shape)
-    # print(excel_transaction.head())
-
-    #transactions = []
     try:
-        with open(file_xlsx, encoding='utf-8') as file:
+        with open(file_xlsx, encoding='utf-8'):
             excel_transaction = pd.read_excel(file_xlsx)
-            print(type(excel_transaction.to_dict("records")))
             return excel_transaction.to_dict("records")
-            # for row in excel_transaction:
-            #     transactions.append(row)
-
-        # return transactions
     except FileNotFoundError:
         print(f"Ошибка: файл {file_xlsx} не найден! ")
         return []
     except Exception as e:
-        print(f"Ошибка при чтении CSV: {e}")
+        print(f"Ошибка при чтении XLSX: {e}")
         return []
 
-# Пример использования
+
 if __name__ == '__main__':
-    # csv_transactions = get_transactions_file_csv("../transactions.csv")
+    # csv_transactions = get_transactions_file_csv(PATH_CSV)
     # print(csv_transactions)
-    xlsx_transactions = get_transactions_file_xlsx("../transactions_excel.xlsx")
+    xlsx_transactions = get_transactions_file_xlsx(PATH_XLSX)
     print(xlsx_transactions)
