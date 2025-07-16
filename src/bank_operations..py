48f57b02e9e0451b29d79ed4cc2f26e2de0b9c95
shura_1986@mail.ru <shura_1986@mail.ru>
2025-07-17 00:31:33 +0300

    written function process bank operations

--- src/bank_operations..py
+++ src/bank_operations..py
@@ -14,12 +14,37 @@ def process_bank_search(data:list[dict], search:str)->list[dict]:
         return []
 
 
-if __name__ == '__main__':
-    operations = [
-        {"amount": 500, "description": "Оплата кафе"},
-        {"amount": 1000, "description": "Перевод другу"},
-        {"amount": 300, "description": "Покупка в МАГАЗИНЕ"},
-    ]
-    found = process_bank_search(operations, "маг")
-    print(found)
 
+def process_bank_operations(data:list[dict], categories:list)->dict:
+    """  Считает количество операций в каждой категории. """
+    counts = {}
+    for operation in data:
+        description = operation.get('description', '').lower()
+        for category in categories:
+            if category.lower() in description:
+                counts[category] = counts.get(category, 0) + 1
+    return counts
+
+
+
+
+# if __name__ == '__main__':
+#     operations = [
+#         {"amount": 500, "description": "Оплата кафе"},
+#         {"amount": 1000, "description": "Перевод другу"},
+#         {"amount": 300, "description": "Покупка в МАГАЗИНЕ"},
+#     ]
+#     found = process_bank_search(operations, "перевод")
+#     print(found)
+
+
+# Пример использования:
+# data = [
+#     {'id': 1, 'amount': 1000, 'description': 'food'},
+#     {'id': 2, 'amount': 500, 'description': 'transport'},
+#     {'id': 3, 'amount': 2000, 'description': 'entertainment'},
+#     {'id': 4, 'amount': 300, 'description': 'food'}
+# ]
+# categories = ['food', 'transport']
+# result = process_bank_operations(data, categories)
+# print(result)
